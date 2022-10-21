9a3d03ca93b3f7282b35558d2c76e31d637a5ce6
SZ-Ives <ives.rementina@talentamp.com>
2022-10-21 09:08:49 +0800

    Completed

--- src/com/bloomtech/welcomeletter/fileIO/FileManager.java
+++ src/com/bloomtech/welcomeletter/fileIO/FileManager.java
@@ -1,19 +1,45 @@
 package com.bloomtech.welcomeletter.fileIO;
+import com.bloomtech.welcomeletter.models.Employee;
+import com.bloomtech.welcomeletter.services.FormLetterService;
 
 import java.io.*;
 import java.util.Scanner;
-
 public class FileManager {
     public FileManager() {
     }
 
     public String getTextFromFile(String filename) {
+        File file = new File("src/resources/" + filename + ".txt");
         String returnString = "";
-
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (line != null) {
+                returnString += line + "\n";
+                line = br.readLine();
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return returnString;
     }
 
     public void writeTextToFile(String outFileName, String text) {
+        File file = new File("src/resources/out/" + outFileName + ".txt");
+        try {
 
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(text);
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/bloomtech/welcomeletter/services/FormLetterService.java
+++ src/com/bloomtech/welcomeletter/services/FormLetterService.java
@@ -1,18 +1,24 @@
 package com.bloomtech.welcomeletter.services;
-
 import com.bloomtech.welcomeletter.fileIO.FileManager;
 import com.bloomtech.welcomeletter.models.Employee;
-
+import java.io.*;
 import java.io.IOException;
-
 public class FormLetterService {
     private static final FileManager fileManager = new FileManager();
 
     public void generateWelcomeLetter(Employee employee) throws IOException {
         //TODO: Get template text from resources
-
+        String txt = fileManager.getTextFromFile("LetterTemplate");
         //TODO: Replace []ed text with correct info
 
+        txt = txt.replace("[company name]", employee.getCompany().getCompanyname())
+                .replace("[fullname]", employee.getFirstname() + " " + employee.getLastname())
+                .replace("[startdate]", String.valueOf(employee.getStartdate()))
+                .replace("[startingsalary]", String.valueOf(employee.getSalary()))
+                .replace("[company starttime]", employee.getCompany().getCompanyname());
+
+
         //TODO: Write finalized String to file
+        fileManager.writeTextToFile("WelcomeTestEmployee", txt);
     }
 }
